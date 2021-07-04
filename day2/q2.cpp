db5564fdb0dad364291475adb0f784f7ccc2f9b8
ashutosh-ve <79149273+ashutosh-ve@users.noreply.github.com>
2021-07-04 08:52:14 +0530

    Create q2.cpp

---
+++ day2/q2.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int numIdenticalPairs(vector<int>& nums) {
+        int n=nums.size();
+        int ans=0;
+        for(int i=0;i<n-1;i++){
+            for(int j=i;j<n;j++){
+                if(nums[i]==nums[j] && i<j)
+                    ans+=1;
+                
+                
+            }
+        }
+ return ans;       
+    }
+};
